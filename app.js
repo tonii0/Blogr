ce3445c75273f865e34ddd147523606b779e46fa
tonii0 <tonibusatta@gmail.com>
2021-04-05 12:51:24 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+gsap.from('.logo', {opacity: 0, duration: 1, delay:1, y: 10})
+gsap.from('.dropdown', {opacity: 0, duration: 1, delay: 1.5, y: 30, stagger: 0.2,})
+gsap.from('.log', {opacity: 0, duration: 1, delay: 2, y: 30})
+gsap.from('.sign', {opacity: 0, duration: 1, delay:2.3, y: 30})
+
+gsap.from('.top-txt', {opacity: 0, duration: 1, delay:2.5, y: 30})
+gsap.from('.button', {opacity: 0, duration: 1, delay:2.8, y: 30})
+gsap.from('.btn-2', {opacity: 0, duration: 1, delay:2.6, y: 30})
+gsap.from('.titre', {opacity: 0, duration: 1, delay:3, x: -30})
